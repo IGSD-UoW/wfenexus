9c47cee307b80c323d31b2e0cf47adc84cc597e1
Carlos Cámara <carlos.camara@warwick.ac.uk>
2021-05-05 16:53:00 +0100

    Refactors data_prep_slupsk.R to make it more re-usable.

--- slupsk/data_prep_slupsk.R
+++ slupsk/data_prep_slupsk.R
@@ -1,15 +1,40 @@
-library(tidyverse)
+# Generates the required datasets for the visualisations.
 
-water_fp_crops <- ci_wf_prep_crops("Report47-Appendix-II.xlsx",
-                              skip_rows = 3, country = "Poland")
-
-write.csv(water_fp_crops, file = here::here("slupsk/data/wf_crops.csv"))
+library(wfenexus)
 
 
-water_fp_animals <- ci_wf_prep_animals("Report48-Appendix-V.xlsx",
-                                       country = "Poland")
+# Variables ---------------------------------------------------------------
 
-write.csv(water_fp_animals, file = here::here("slupsk/data/wf_animals.csv"))
+sel_country <- "Poland"
+calc_wf <- FALSE
+
+
+# Generate waterfootprint files. ------------------------------------------
+
+if (calc_wf == TRUE) {
+  water_fp_crops <- ci_wf_prep_crops("Report47-Appendix-II.xlsx",
+                                     skip_rows = 3, country = sel_country)
+
+  write.csv(water_fp_crops, file = here::here("slupsk/data/wf_crops.csv"))
+
+
+  water_fp_animals <- ci_wf_prep_animals("Report48-Appendix-V.xlsx",
+                                         country = sel_country)
+
+  write.csv(water_fp_animals, file = here::here("slupsk/data/wf_animals.csv"))
+}
 
 
 
+
+# Adds wf to ingredients --------------------------------------------------
+
+water_fp_crops <- read_csv(here::here("slupsk/data/wf_crops.csv"))
+water_fp_animals <- read_csv(here::here("slupsk/data/wf_animals.csv"))
+
+
+ingredients_df <- read_csv(
+  here::here("slupsk/data-raw/ingredient_ingredient.csv"))
+
+# @TODO: create a function to merge water footprint only from crops and animals
+# with ingredients.
