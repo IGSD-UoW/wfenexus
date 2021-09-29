847ccce8f7dc573bad638890f0f45334c8387167
Carlos Cámara <carlos.camara@warwick.ac.uk>
2021-09-29 16:10:24 +0100

    Task #20 - Starts translations.

---
+++ slupsk/locale/en.R
@@ -0,0 +1,78 @@
+# Strings translations.
+
+# Edit the strings between "", making sure to preserve the double ".
+
+
+parents_string <- "Parents"
+children_string <- "Children"
+kindergarten_string <- "Kindergarten"
+
+# Overview ----------------------------------------------------------------
+
+# Value boxes.
+num_kindergartens <- "Kindergartens involved in the project."
+num_dishes <- "Number of dishes."
+num_ingredients <- "Number of ingredients."
+
+
+# Table and filters -------------------------------------------------------
+
+name_string <- "Name"
+calories_string <- "Calories"
+waste_string <- "Waste"
+
+ingredient_string <- "Ingredient"
+
+meal_type_string <- "Type"
+
+
+rating_children_string <- "Rating (Children)"
+rating_parents_string <- "Rating (Parents)"
+rating_average_string <- "Rating (Average)"
+
+health_string <- "Perceived health"
+health_string_long <-  "Average of parents' ratings on perceived health. High values do not necessarily mean that the meal is healthy."
+
+water_used_string <- "Used Water"
+water_used_tooltip <- "Total Water Used. Units: m3"
+
+water_saving_string <- "Water Saving"
+water_saving_tooltip <- "Difference between the current choice of ingredients compared to the same ingredients being produced elsewhere. Negative values mean reduction of water, and therefore, desirable."
+
+water_saving_potential_string <- "Potential water Saving"
+water_saving_potential_tooltip <- "Difference between the current choice of ingredients compared to the same ingredients being produced locally. Higher values represent greater room for improvement."
+
+emissions_transport_string <- "Transport emissions"
+
+co2_emissions_string <- "CO2 Emissions"
+co2_emissions_string_long <- "Total CO2 emitted in transportation. Units: kgCO2e"
+
+from_producer_string <- "Produced in Poland?"
+
+weight_string <- "Weight (g)"
+
+wfp_avg_string <- "Water Footprint (Avg)"
+wfp_avg_tooltip <- "Total Water Footprint, using world averages. Units: m3/Ton"
+
+wfp_poland_string <- "Water Footprint (PL)"
+wfp_poland_tooltip <- "Total Water Footprint, assuming it was produced in Poland. Units: m3/Ton"
+
+
+# Food --------------------------------------------------------------------
+
+# Plots
+top5_kindergartens_title <- "Top 5 kindergartens by food rating"
+top5_kindergartens_subtitle <- "Based on the average ratings made by parents and children"
+
+top5_breakfast_title <-"Top 5 breakfast dishes by food rating"
+top5_afternoon_title <- "Top 5 afternoon tea dishes"
+
+ing_usg_wft_title <- "Ingredients' usage vs. water footprint"
+ing_usg_wft_subtitle <- ""
+ing_usg_wft_x <- "Total weight (g)"
+ing_usg_wft_y <-"Avg. Water Footprint (m3/Ton)"
+ing_usg_wft_color <- "Type of food"
+
+comparison_ratings_title <- "Comparison of rating made by parents and children"
+comparison_ratings_subtitle <- top5_kindergartens_subtitle
+
